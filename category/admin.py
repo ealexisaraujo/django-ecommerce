49998cb53df0c6a0568b6fbe7041cc356b54b8ba
Alexis Araujo <ala1289@gmail.com>
2021-11-30 16:31:13 -0600

    :sparkles:  Pre-populate Category Slug

--- category/admin.py
+++ category/admin.py
@@ -2,4 +2,14 @@ from django.contrib import admin
 from . import models
 
 # Register your models here.
-admin.site.register(models.Category)
+
+class CategoryAdmin(admin.ModelAdmin):
+    """
+    Customizing the admin page for the category model
+    """
+    prepopulated_fields = {'slug': ('category_name',)}
+    list_display = ('category_name','slug')
+    list_display_links = ('category_name', 'slug')
+    search_fields = ('category_name', 'slug')
+
+admin.site.register(models.Category, CategoryAdmin)

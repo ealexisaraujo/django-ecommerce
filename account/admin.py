daadcc4f5810053cad35325e44855d7b472be1c5
Alexis Araujo <ala1289@gmail.com>
2021-11-30 14:12:48 -0600

    :sparkles: Making the custom password field read-only

--- account/admin.py
+++ account/admin.py
@@ -1,5 +1,21 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from . import models
 
 # Register your models here.
-admin.site.register(models.Account)
+
+class AccountAdmin(UserAdmin):
+    """
+    Customizing the admin page for the user model
+    """
+    list_display = ('email', 'first_name', 'last_name', 'username', 'last_login','date_joined','is_active')
+    list_display_links = ('email', 'first_name', 'last_name')
+    search_fields = ('email', 'username')
+    readonly_fields = ('date_joined', 'last_login')
+    ordering =('-date_joined',)
+
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets = ()
+
+admin.site.register(models.Account, AccountAdmin)

fa52e4116ac3202d418c8508229c2de960dceffe
Alexis Araujo <ala1289@gmail.com>
2021-11-30 18:03:45 -0600

    :sparkles: Make Store Page

--- greatkart/urls.py
+++ greatkart/urls.py
@@ -17,11 +17,13 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.urls.conf import include
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+    path("store/", include("store.urls")),
 ]
 
 if bool(settings.DEBUG):

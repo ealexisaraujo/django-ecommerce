cde90d317acbc5d0117b9751619bf8f0fbccacda
Alexis Araujo <ala1289@gmail.com>
2022-01-11 21:30:05 -0600

    ✨ Add cart admin and cart item admin

--- carts/admin.py
+++ carts/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
 from .models import Cart, CartItem
 
+class CartAdmin(admin.ModelAdmin):
+    list_display = ['id', 'cart_id', 'date_added']
+    list_display_links = ['id', 'cart_id', 'date_added']
+    list_filter = ['date_added']
+    search_fields = ['cart_id']
+
+class CartItemAdmin(admin.ModelAdmin):
+    list_display = ['product', 'cart', 'quantity', 'is_active']
+
 # Register your models here.a
-admin.site.register(Cart)
-admin.site.register(CartItem)
+admin.site.register(Cart, CartAdmin)
+admin.site.register(CartItem,CartItemAdmin)

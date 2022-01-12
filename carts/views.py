ddd2913df94fca3f380e56427b81338d25082a2a
Alexis Araujo <ala1289@gmail.com>
2022-01-12 12:15:35 -0600

    ✨ Add variation in cart item
    #

--- carts/views.py
+++ carts/views.py
@@ -36,6 +36,10 @@ def add_cart(request, product_id):
 
     try:
       cart_item = CartItem.objects.get(product=product, cart=cart)
+      if len(product_variation) > 0:
+        cart_item.variations.clear()
+        for item in product_variation:
+          cart_item.variations.add(item)
       if cart_item.quantity < cart_item.product.stock:
         cart_item.quantity += 1
       cart_item.save()
@@ -45,6 +49,10 @@ def add_cart(request, product_id):
         quantity = 1,
         cart = cart
       )
+      if len(product_variation) > 0:
+        cart_item.variations.clear()
+        for item in product_variation:
+          cart_item.variations.add(item)
       cart_item.save()
     return redirect('cart')
 
